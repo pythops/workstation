9d226717039db9cf56572670e9ee036db69c1745
pythops <contact@pythops.com>
2023-09-08 21:11:07 +0200

    format shell scripts

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -5,39 +5,39 @@ set -e
 target=$1
 
 available_targets=(
-	"all"
-	"containers"
-	"dnsmasq"
-	"esp"
-	"git"
-	"gpu"
-	"i3"
-	"keyboard"
-	"minikube"
-	"neovim"
-	"npm"
-	"packages"
-	"python"
-	"rust"
-	"starship"
-	"tmux"
-	"wezterm"
-	"yay"
-	"zathura"
-	"zsh"
+    "all"
+    "containers"
+    "dnsmasq"
+    "esp"
+    "git"
+    "gpu"
+    "i3"
+    "keyboard"
+    "minikube"
+    "neovim"
+    "npm"
+    "packages"
+    "python"
+    "rust"
+    "starship"
+    "tmux"
+    "wezterm"
+    "yay"
+    "zathura"
+    "zsh"
 )
 
 if echo "${available_targets[@]}" | grep -qw "$target"; then
-	if [ -v SUDO_WITHOUT_PASSWORD ]; then
-		ansible-playbook configure.yaml -t "$1"
-	else
-		ansible-playbook configure.yaml -K -t "$1"
-	fi
+    if [ -v SUDO_WITHOUT_PASSWORD ]; then
+        ansible-playbook configure.yaml -t "$1"
+    else
+        ansible-playbook configure.yaml -K -t "$1"
+    fi
 else
-	echo "Unknown target"
-	echo "the available targets are:"
-	for t in "${available_targets[@]}"; do
-		echo "$t"
-	done
-	exit 1
+    echo "Unknown target"
+    echo "the available targets are:"
+    for t in "${available_targets[@]}"; do
+        echo "$t"
+    done
+    exit 1
 fi
