c7c457827da5ae58682d8dcfe25ee79d83bcae2a
pythops <contact@pythops.com>
2023-09-23 17:49:52 +0200

    nvim : fix lsp error when using Tab

--- roles/configure/files/config/nvim/lua/plugins/lsp.lua
+++ roles/configure/files/config/nvim/lua/plugins/lsp.lua
@@ -72,7 +72,7 @@ return {
     local cmp = require("cmp")
 
     local has_words_before = function()
-      local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
