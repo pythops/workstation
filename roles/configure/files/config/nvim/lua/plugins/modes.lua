b4c420854e9cb9e7a67403192c5b8e870305ecec
pythops <contact@pythops.com>
2023-09-23 18:11:54 +0200

    nvim: update modes.nvim colors for visual mode

--- roles/configure/files/config/nvim/lua/plugins/modes.lua
+++ roles/configure/files/config/nvim/lua/plugins/modes.lua
@@ -2,6 +2,10 @@ return {
   "mvllow/modes.nvim",
   tag = "v0.2.0",
   config = function()
-    require("modes").setup()
+    require("modes").setup({
+      colors = {
+        visual = "#fe8019",
+      },
+    })
   end,
 }
