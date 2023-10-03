2bcc70fdfaadb044fcce5b57c7291b0048544f44
pythops <contact@pythops.com>
2023-10-03 09:48:42 +0200

    nvim: add signs for lsp

--- roles/configure/files/config/nvim/lua/plugins/lsp.lua
+++ roles/configure/files/config/nvim/lua/plugins/lsp.lua
@@ -12,6 +12,12 @@ return {
   },
 
   config = function()
+    local signs = { Error = "❌", Warn = "⚠️", Hint = "💡", Info = "" }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
     vim.diagnostic.config({
       virtual_text = false,
       signs = true,
