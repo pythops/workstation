72000b477a4ee846e3166f506c5bd01b14d54105
pythops <contact@pythops.com>
2023-11-09 19:13:47 +0100

    nvim: fix nvim spider config

--- roles/configure/files/config/nvim/lua/plugins/nvim-spider.lua
+++ roles/configure/files/config/nvim/lua/plugins/nvim-spider.lua
@@ -2,7 +2,7 @@
 return {
   "chrisgrieser/nvim-spider",
   config = function()
-    require("spider").setup()
+    require("spider").setup({})
     vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
     vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
     vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
