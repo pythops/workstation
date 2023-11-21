7e4ca3ad91b0a1e08863a3643c934aff38113459
pythops <contact@pythops.com>
2023-11-21 22:56:24 +0100

    nvim: add mypy to linters list for python

--- roles/configure/files/config/nvim/lua/plugins/nvim-lint.lua
+++ roles/configure/files/config/nvim/lua/plugins/nvim-lint.lua
@@ -3,7 +3,7 @@ return {
   "mfussenegger/nvim-lint",
   config = function()
     require("lint").linters_by_ft = {
-      python = { "ruff" },
+      python = { "ruff", "mypy" },
       yaml = { "yamllint" },
       markdown = { "vale" },
       dockerfile = { "hadolint" },
