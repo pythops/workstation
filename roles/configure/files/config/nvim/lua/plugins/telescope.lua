7d612c471affdf4087ab0ea2cfa6dc95fb7c0818
pythops <contact@pythops.com>
2024-06-25 16:58:11 +0200

    nvim: update telescope version

--- roles/configure/files/config/nvim/lua/plugins/telescope.lua
+++ roles/configure/files/config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.4",
+  tag = "0.1.8",
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
