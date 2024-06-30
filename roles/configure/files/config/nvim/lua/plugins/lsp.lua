f9176048a4bac5d4390c2cf79cd80d649feeda7f
pythops <contact@pythops.com>
2024-06-30 13:54:23 +0200

    nvim: fix Tab autocompletion error

--- roles/configure/files/config/nvim/lua/plugins/lsp.lua
+++ roles/configure/files/config/nvim/lua/plugins/lsp.lua
@@ -84,7 +84,8 @@ return {
     local cmp = require("cmp")
 
     local has_words_before = function()
-      local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
