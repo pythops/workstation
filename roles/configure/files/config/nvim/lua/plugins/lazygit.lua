79dbc81d6354051ac161f79aaf2b5ca91145577b
Badr <contact@pythops.com>
2025-07-20 19:00:22 +0200

    neovim: update lazygit config

--- roles/configure/files/config/nvim/lua/plugins/lazygit.lua
+++ roles/configure/files/config/nvim/lua/plugins/lazygit.lua
@@ -2,6 +2,6 @@ return {
   "kdheepak/lazygit.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    vim.api.nvim_set_keymap("n", "<leader>l", ":LazyGit<CR>", { silent = true })
+    vim.api.nvim_set_keymap("n", "<leader>l", "<cmd>LazyGit<cr>", { silent = true })
   end,
 }
